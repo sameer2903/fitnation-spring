77326a01bb02e0a523e2e870873f7217ce13249b
rubi314 <rubi.roy@siemens.com>
2024-10-08 19:17:09 +0530

    springboot commit

---
+++ fitnation/src/test/java/com/example/fitnation/FitnationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fitnation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
